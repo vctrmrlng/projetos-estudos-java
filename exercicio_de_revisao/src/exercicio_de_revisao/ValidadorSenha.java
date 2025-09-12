b5eff197715b529395d49358383ee5c2a742be28
Victor M. <vmerling02@gmail.com>
2025-09-12 11:36:32 -0300

    Aula 2025-09-12

---
+++ exercicio_de_revisao/src/exercicio_de_revisao/ValidadorSenha.java
@@ -0,0 +1,36 @@
+package exercicio_de_revisao;
+
+import java.util.Scanner;
+
+public class ValidadorSenha {
+
+	private String senha;
+	
+	public boolean TestaMaiuscula() {
+		String maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		for (int i=0; i< senha.length(); i++)
+			for (int j=0; j < maiusculas.length();j++) {
+				if (this.senha.charAt(i) == maiusculas.charAt(j))
+					return true}
+	}
+	
+	public boolean verificarSeguranca() {
+			if (this.senha.length() < 8) {
+				return false;
+			}
+			else if (this.senha.equals(this.senha.toLowerCase())) {
+				return false;
+			}
+			else {return true;
+			}
+	}
+
+	public String getSenha() {		
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
